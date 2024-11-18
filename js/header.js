a24efd1e792adec45449b02d49cd1df49cf2693f
Spedy22 <nikolayshavro2005@gmail.com>
2024-11-18 12:42:04 +0200

    Finish about section

--- js/header.js
+++ js/header.js
@@ -25,6 +25,11 @@
     openModalBtn5: document.querySelector("[data-modal-info-3-open]"),
     closeModalBtn5: document.querySelector("[data-modal-info-3-close]"),
     modal5: document.querySelector("[data-modal-info-3]"),
+
+    // Для шостої модалки (відео)
+    openModalBtn6: document.querySelector("[data-modal-video-open]"),
+    closeModalBtn6: document.querySelector("[data-modal-video-close]"),
+    modal6: document.querySelector("[data-modal-video]"),
   };
 
   // Перша модалка
@@ -50,6 +55,10 @@
   refs.openModalBtn5.addEventListener("click", toggleModal5);
   refs.closeModalBtn5.addEventListener("click", toggleModal5);
 
+  // Шоста модалка (відео)
+  refs.openModalBtn6.addEventListener("click", toggleModal6);
+  refs.closeModalBtn6.addEventListener("click", toggleModal6);
+
   // Функція для першої модалки
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
@@ -74,54 +83,9 @@
   function toggleModal5() {
     refs.modal5.classList.toggle("is-hidden");
   }
+
+  // Функція для шостої модалки (відео)
+  function toggleModal6() {
+    refs.modal6.classList.toggle("is-hidden");
+  }
 })();
-
-
-
-// (() => {
-//   const refs = {
-//     // Для першої модалки
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-
-//     // Для другої модалки
-//     openModalBtn2: document.querySelector("[data-modal-open-2]"),
-//     openModalBtn2Another: document.querySelector("[data-modal-open-2-another]"),
-//     closeModalBtn2: document.querySelector("[data-modal-close-2]"),
-//     modal2: document.querySelector("[data-modal-2]"),
-
-//     // Для третьої модалки
-//     openModalBtn3: document.querySelector("[data-modal-info-1-open]"),
-//     closeModalBtn3: document.querySelector("[data-modal-info-1-close]"),
-//     modal3: document.querySelector("[data-modal-info-1]"),
-//   };
-
-//   // Перша модалка
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   // Друга модалка
-//   refs.openModalBtn2.addEventListener("click", toggleModal2);
-//   refs.openModalBtn2Another.addEventListener("click", toggleModal2);
-//   refs.closeModalBtn2.addEventListener("click", toggleModal2);
-
-//   // Третя модалка
-//   refs.openModalBtn3.addEventListener("click", toggleModal3);
-//   refs.closeModalBtn3.addEventListener("click", toggleModal3);
-
-//   // Функція для першої модалки
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-
-//   // Функція для другої модалки
-//   function toggleModal2() {
-//     refs.modal2.classList.toggle("is-hidden");
-//   }
-
-//   // Функція для третьої модалки
-//   function toggleModal3() {
-//     refs.modal3.classList.toggle("is-hidden");
-//   }
-// })();
